1683ae086cdef02920a449dcd8a5b6b98a603995
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-07 09:14:58 -0900

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  * Return: 0
  */
 void print_alphabet(void)
-{
+{      
         char c;
 
         for (c = 'a'; c <= 'z'; c++)
         {
                 _putchar(c);
         }
+        
 _putchar('\n');
 }
